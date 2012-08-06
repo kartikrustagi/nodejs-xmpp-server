8027531f83ac95e67f9d8770367be921cfc5ad23
Kartik Rustagi <kartikrustagi@gmail.com>
2012-08-06 23:09:57 +0530

    Untested auth code

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+	//Mongodb configuration
+	mongo_db: 'projectX',
+	mongo_port: 27017,
+	mongo_domain: '127.0.0.1',
+
+	//XMPP server configuration
+	port: 5222,
+	domain: '0.0.0.0'
+}
---
+++ xmpp-server.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+//Global Application object
+PROJECTX = {};
+
+//Setting up application wide logging
+var logger = require('winston');
+PROJECTX.logger = logger;
+
+var assert = require('assert');
+PROJECTX.assert = assert;
+
+//Setting config
+var path = require('path'), config = require('jsconfig'), configFilePath = path.join(__dirname, 'config.js');
+config.load(configFilePath, function () {
+	PROJECTX.config = config;
+	logger.info("Configuration file loaded");
+});
+
+
+(function () {
+	//Initializing DB connection
+	var mongo = require('mongodb'), db = new mongo.Db(PROJECTX.config.mongo_db, new mongo.Server(PROJECTX.config.mongo_domain, PROJECTX.config.mongo_port));
+	logger.info("Mongo DB object created");
+	PROJECTX.db = db;
+}());
+
+var server = require('./lib/server.js');
+server.run(config, function() {
+	logger.info("Server ready");
+});
