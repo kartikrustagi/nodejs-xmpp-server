cf0042b4983ae0c26efc0f514481722e87830551
Kartik Rustagi <kartikrustagi@gmail.com>
2013-01-15 23:35:08 +0530

    Working roster

--- modules/roster.js
+++ modules/roster.js
@@ -1,8 +1,8 @@
 var xmpp = require('node-xmpp');
 var r = require('../lib/roster.js');
 var ltx = require('ltx');
-var RosterStorage = r.Roster;
-var RosterContactStorage = r.RosterContact;
+var Roster = r.Roster;
+var Contact = r.Contact;
 var logger = PROJECTX.logger;
 
 // http://xmpp.org/rfcs/rfc3921.html#roster
@@ -18,24 +18,13 @@ var logger = PROJECTX.logger;
 exports.configure = function(server, config) {
 
     server.on("connect", function(client) {
-        client.roster = new RosterStorage();
-
-        client.roster.on('add', function(item) {
-            // <iq type='set'>
-            //   <query xmlns='jabber:iq:roster'>
-            //     <item
-            //         jid='contact@example.org'
-            //         subscription='none'
-            //         ask='subscribe'
-            //         name='MyContact'>
-            //       <group>MyBuddies</group>
-            //     </item>
-            //   </query>
-            // </iq>
-        });
 
         client.on('auth-success', function(jid) {
-            client.roster.owner = jid.bare().toString();
+            Roster.find(jid, function(roster) {
+				client.roster = roster;
+				console.log("In Roster auth-success: ");
+				console.log(client.roster);
+			});
         });
 
         client.on('stanza', function(stz) {
@@ -43,59 +32,54 @@ exports.configure = function(server, config) {
             var stanza = ltx.parse(stz.toString());
             var query = null;
             if (stanza.is('iq') && (query = stanza.getChild('query', "jabber:iq:roster"))) {
+				console.log("In stanza roster: ");
+				console.log(client.roster);
                 if(stanza.attrs.type === "get") {
 					logger.info("Roster get request from : "+stanza.attrs.from);
                     stanza.attrs.type = "result";
-                    RosterStorage.find(new xmpp.JID(stanza.attrs.from).bare().toString(), function(roster) {
-                        roster.contacts.forEach(function(contact) {
-                            query.c("item", {jid: contact.jid, name: contact.name, subscription: contact.state});
-                        });
-                        stanza.attrs.to = stanza.attrs.from;
-                        client.send(stanza); 
-                    });
-                }
-                else if(stanza.attrs.type === "set") {
+					for(contactJidBStr in roster.contacts) {
+						if(roster.contacts.hasOwnProperty(contactJidBStr)) {
+							var contact = roster.contacts[contactJidBStr];
+							query.c("item", {jid: contact.contactJid, name: contact.name, subscription: contact.subscription});
+						}
+					}
+					stanza.attrs.to = stanza.attrs.from;
+					client.send(stanza); 
+                } else if(stanza.attrs.type === "set") {
                     stanza.attrs.type = "result";
                     var i = query.getChild('item', "jabber:iq:roster");
-                    RosterStorage.find(new xmpp.JID(stanza.attrs.from).bare().toString(), function(roster) {
-                        RosterContactStorage.find(roster, new xmpp.JID(i.attrs.jid).bare().toString(), function(contact) {
-                            if(i.attrs.subscription === "remove") {
-                                contact.delete(function() {
-                                    // And now send to all sessions.
-                                    i.attrs.subscription = 'remove';
-                                    stanza.attrs.from = client.server.options.domain; // Remove the from field.
-                                    client.server.router.connectedClientsForJid(client.jid.toString()).forEach(function(jid) {
-                                        stanza.attrs.to = jid.toString();
-                                        client.send(stanza); // TODO: Blocking Outbound Presence Notifications.
-                                    });
-                                });
-                            } else {
-                                if(contact.state === "from" && i.attrs.subscription === "to") {
-                                    contact.state = "both";
-                                }
-                                else if(contact.state === "to" && i.attrs.subscription === "from") {
-                                    contact.state = "both";
-                                }
-                                else {
-                                    contact.state = i.attrs.subscription || "to";
-                                }
-                                contact.name = i.attrs.name || i.attrs.jid;
-                                contact.save(function() {
-                                    // And now send to all sessions.
-                                    i.attrs.subscription = contact.state;
-                                    stanza.attrs.from = client.server.options.domain; // Remove the from field.
-                                    client.server.router.connectedClientsForJid(client.jid.toString()).forEach(function(jid) {
-                                        stanza.attrs.to = jid.toString();
-                                        client.send(stanza); // TODO: Blocking Outbound Presence Notifications.
-                                    });
-                                });
-                            }
-                        });
-                    });
-                } else if(stanza.attrs.type === "result") {
-                    // Not much!
-                }
-            }
+					if(i.attrs.subscription === "remove") {
+						client.roster.deleteContact(new xmpp.JID(i.attrs.jid), function(err) {
+							var resultStanza = null;
+							if(err == null) {
+								resultStanza = new xmpp.Element('iq', {'to' : stanza.attrs.from, 'type' : 'result', 'id' : stanza.attrs.id});
+							}else {
+								resultStanza = new xmpp.Element('iq', {'to' : stanza.attrs.from, 'type' : 'error', 'id' : stanza.attrs.id});
+								resultStanza.c("error").c('text', { xmlns: "urn:ietf:params:xml:ns:xmpp-stanzas" }).t(err.message);
+							}
+							console.log("Post deleteContact roster");
+							console.log(client.roster);
+							client.send(resultStanza);
+						});
+					} else {
+						// update or add contact request
+						client.roster.addContact(new xmpp.JID(i.attrs.jid), i.attrs.name, function(err) {
+							var resultStanza = null;
+							if(err == null) {
+								resultStanza = new xmpp.Element('iq', {'to' : stanza.attrs.from, 'type' : 'result', 'id' : stanza.attrs.id});
+							}else {
+								resultStanza = new xmpp.Element('iq', {'to' : stanza.attrs.from, 'type' : 'error', 'id' : stanza.attrs.id});
+								resultStanza.c("error").c('text', { xmlns: "urn:ietf:params:xml:ns:xmpp-stanzas" }).t(err.message);
+							}
+							console.log("Post addContact Roster");
+							console.log(client.roster);
+							client.send(resultStanza);
+						});
+						
+					}
+				}
+			}
         });
+
     });
 }
